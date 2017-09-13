fc3418022182e0a6d7b2ee35a62167efa885bfb9
gemetalreg <itunereg@gmail.com>
2017-09-13 14:43:49 +0300

    Update get_youtube_playlist.py

--- get_youtube_playlist.py
+++ get_youtube_playlist.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import os
 from pytube import YouTube
 import argparse
-from multiprocessing import Process
+from multiprocessing import Process, freeze_support
 
 def get_links_from_youtube(palylist_url) -> set:
     video_link_set = set()
@@ -15,9 +15,9 @@ def get_links_from_youtube(palylist_url) -> set:
         list_a = html_list.body.findAll("a")
 
         for link in list_a:
-            temp = link["href"]
+            temp = "https://www.youtube.com" + link["href"]
             if "index" in temp and temp not in video_link_set:
-                video_link_set.add("https://www.youtube.com" + temp)
+                video_link_set.add(temp)
 
     return video_link_set
 
@@ -25,9 +25,11 @@ def get_video_format() -> str:
     return "mp4"
 
 def get_video_resolution(youtube: YouTube, video_format) -> str:
-    resolutions = youtube.filter(video_format)
-    return resolutions[-1]
-
+    resolutions = ["1080p", "720p", "480p", "360p", "240p"]
+    for res in resolutions:
+        if youtube.filter(video_format, resolution = res):
+            return res
+    raise TypeError("choose another video format")
 
 def get_file_of_urls(palylist_url, path):
     '''
@@ -40,9 +42,10 @@ def get_file_of_urls(palylist_url, path):
     if not os.path.exists(path):
         os.mkdir(path)
 
+    procs = []
     for link in video_link_set:
         yt = YouTube(link.rstrip())
-        
+
         video_format = get_video_format()
 
         video_resolution = get_video_resolution(yt, video_format)
@@ -50,8 +53,13 @@ def get_file_of_urls(palylist_url, path):
         video = yt.get(extension = video_format, resolution = video_resolution)
 
         video_download_process = Process(target = video.download, args = (path,))
-        video_download_process.start()
-        video_download_process.join()
+        procs.append(video_download_process)
+
+    for process in procs:
+        process.start()
+
+    for process_joinable in procs:
+        process_joinable.join()
 
 parser = argparse.ArgumentParser()
 
@@ -69,4 +77,6 @@ palylist_url = args.palylist_url[0]
 
 dest = args.dest[0]
 
-get_file_of_urls(palylist_url, dest)
+if __name__ == '__main__':
+    freeze_support()
+    get_file_of_urls(palylist_url, dest)
